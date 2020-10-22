3263c5afece887fb307d870198ba352762b952e3
Alexander Voloshok <alexandervoloshok@gmail.com>
2020-10-22 09:07:46 +0300

    Add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { LeafletModule } from '@asymmetrik/ngx-leaflet';
 
 import { AppComponent } from './app.component';
+import { MapComponent } from './map/map.component';
+import { TimelineComponent } from './timeline/timeline.component';
+import { HeaderComponent } from './header/header.component';
+import { LegendComponent } from './legend/legend.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapComponent,
+    TimelineComponent,
+    HeaderComponent,
+    LegendComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    LeafletModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/map/map.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { latLng, tileLayer } from 'leaflet';
+
+@Component({
+  selector: 'app-map',
+  templateUrl: './map.component.html',
+  styleUrls: ['./map.component.scss']
+})
+export class MapComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  options = {
+    layers: [
+      tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; OpenStreetMap contributors'
+      })
+    ],
+    zoom: 7,
+    center: latLng([ 46.879966, -121.726909 ])
+  };
+
+}
